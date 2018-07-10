38c43f7d8e5d6f2562a6e33669b38e894d0c289b
Matthew Feickert <matthew.feickert@cern.ch>
2018-07-10 21:22:58 +0200

    Add Binder support (#2)
    * Add Binder support
    
    As outlined in https://github.com/binder-examples/r, have Binder
    use the snapshot of MRAN from the date given in runtime.txt and then
    install the packages that are given in install.R.
    In this case the tidyverse is installed to give a user who is exploring
    some freedom with the packages they choose.
    
    * Add Binder badge to README

---
+++ binder/install.R
@@ -0,0 +1,12 @@
+# Just need ggplot2 for example, but allows for further play
+# install.packages('tidyverse')
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("readr")
+install.packages("purrr")
+install.packages("tibble")
+install.packages("stringr")
+install.packages("forcats")
+
+install.packages("testthat")
